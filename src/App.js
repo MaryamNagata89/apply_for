71d59b657cc0d82e01b89bd3344648a70b6738c6
永田マリ <nagamari80@gmail.com>
2020-07-10 15:52:31 +0900

    usestate 展開

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => {
-    setName(props.name);
-    setPrice(props.price);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+
   return (
     <>
       <div>
         <p>
           現在の{name}は{price}円です。
         </p>
-        <button onClick={() => setPrice(price + 1)}>+1</button>
-        <button onClick={() => setPrice(price - 1)}>-1</button>
-        <button onClick={reset}>Reaset</button>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={() => setState({ ...state, price: price + 1 })}>
+          +1
+        </button>
+        <button onClick={() => setState({ ...state, price: price - 1 })}>
+          -1
+        </button>
+        <button onClick={() => setState(props)}>Reaset</button>
+        <input
+          value={name}
+          onChange={(e) => setState({ ...state, name: e.target.value })}
+        />
       </div>
     </>
   );
