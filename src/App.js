dbff513daa206c82b98dc2e964dae478ae454231
永田マリ <nagamari80@gmail.com>
2020-07-10 15:42:18 +0900

    use many useState

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import React, { useState } from 'react';
-import { prettyDOM } from '@testing-library/react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const reset = () => {
+    setName(props.name);
+    setPrice(props.price);
   };
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  const divide = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
   return (
     <>
       <div>
-        <p>count:{count}</p>
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>*2</button>
-        <button onClick={divide}>3の倍数の時だけ割る</button>
+        <p>
+          現在の{name}は{price}円です。
+        </p>
+        <button onClick={() => setPrice(price + 1)}>+1</button>
+        <button onClick={() => setPrice(price - 1)}>-1</button>
+        <button onClick={reset}>Reaset</button>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
       </div>
     </>
   );
+};
+App.defaultProps = {
+  name: '',
+  price: 1000,
 };
 
 export default App;
